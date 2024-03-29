c9fc0f3d95deb7765244f5fde7838f5ea65d4c1d
HaroldMontanoHurtado <harold.montano@correounivalle.edu.co>
2024-03-28 20:42:53 -0500

    correcion de typeo en el if del main

--- App.py
+++ App.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if(__name__=='main'):
-    app.run(port=3000, debug=True)
+@app.route('/')
+def Index():
+    return 'Hello world'
+
+if(__name__=='__main__'):
+    app.run(port=5500, debug=True)
